e5d5572ed285ddef01707c763d638754bfec3c2e
life0nmars <43827050+life0nmars@users.noreply.github.com>
2018-12-26 22:29:15 +0300

    Create task5.cpp

---
+++ 26.12.2018/task5.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    int M;
+    cin >> M;
+    int N;
+    int *m = new int[N];
+    int *c = new int[N];
+    double *w = new double[N];
+    for(int i = 0; i < N; i++)
+    {
+        cout << "масса: "; cin >> m[i];
+        cout << "цена: "; cin >> c[i];
+        w[i] = m[i] / c[i];
+    }
+    
+    double max = w[0];
+    int j = 0;
+    for(int i = 1; i < N; i++)
+    {
+        if(max < w[i])
+        {
+            max = w[i];
+            j = i;
+        }
+    }
+    
+    double W = M / m[j];
+    cout << "рюкзак можно наполнить вещами стоимостью " << W*c[j];
+
+    return 0;
+}
