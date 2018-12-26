06ec0b5e9bc46e537236bfbb3788e57fd3e10c1a
life0nmars <43827050+life0nmars@users.noreply.github.com>
2018-12-26 22:28:47 +0300

    Create task3.cpp

---
+++ 26.12.2018/task3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib> 
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr = new int[n];
+    for(int i = 0; i < n; i++)
+    cin >> n;
+    int m,l; // para chisel
+    int j = 0; //chislo sovpadeni s paroy
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] == m)
+        j++;
+        if(arr[i] == l)
+        j++;
+    }
+    if(j => 2)
+    cout << "true";
+    else
+    cout << "false";
+return 0;
+}
